17e06d19ad92fbb41e94253933656d1c560b3c87
SerhiiHerashchenko <serhii.herashchenko@stud.onu.edu.ua>
2024-09-04 19:08:29 +0300

    Cycles's been deleted

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        for(int i = 1; i < 10; i++)
-        {
-            System.out.println("forcycle");
-        }
     }
 }
